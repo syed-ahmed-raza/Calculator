c84059d9d349bb5915cfe2dc8ac8d5f38c7660dc
syed-ahmed-raza <ar03110386073@gmail.com>
2024-04-01 23:11:56 +0500

    Created Calculator

---
+++ calculator.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "What is your Name? ", type: "string", name: "Yourname" },
+    {
+        message: "Please Enter First Number: ",
+        type: "number",
+        name: "firstnumber",
+    },
+    {
+        message: "Please Enter Second Number:",
+        type: "number",
+        name: "secondnumber",
+    },
+    {
+        message: "Select one of the operator:",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//   Conditional Statements
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Please select valid operator");
+}
